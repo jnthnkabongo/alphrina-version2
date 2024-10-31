536ad761485418f46ae125fdd966dfee00d4ad51
jnthnkabongo <jnthnkabongo@gmail.com>
2024-10-31 18:32:42 +0100

    ajout

--- src/Pages/AddColis.jsx
+++ src/Pages/AddColis.jsx
@@ -1,8 +1,12 @@
 import { React, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const AddColis = () => {
+    const dispatch = useDispatch();
+    const form = useRef();
+
     const [inputListNew, setInputListNew] = useState([
         { nom_colis: "", nombre_total_colis:"", prix_unitaire: "", kilo_colis: "", prix_total: ""},
     ]);
@@ -17,15 +21,47 @@ const AddColis = () => {
         setInputListNew(list);
     }
     
+    const handleSubmitColis = async (e) => {
+        e.preventdefault();
+        setloading(true);
     
-    // const colis = inputListNew.map((item) => ({
-    //     nom_colis: item.colis,
-    //     nombre_total_colis: item.colis,
-    //     prix_unitaire: item.colis,
-    //     kilo_colis: item.colis,
-    //     prix_total: item.colis
-    // }));
+        const colisData = {
+            nom_colis : form.current["nom_colis"].value,
+            nombre_total_colis : form.current["nombre_total_colis"].value,
+            prix_unitaire : form.current["prix_unitaire"].value,
+            kilo_colis : form.current["kilo_colis"].value,
+            prix_total : form.current["prix_total"].value,
+        };
+        
+        const colis = inputListNew.map((item) => ({
+            nom_colis: item.colis,
+            nombre_total_colis: item.colis,
+            prix_unitaire: item.colis,
+            kilo_colis: item.colis,
+            prix_total: item.colis
+        }));
 
+        const FormData = {
+            ...colisData,
+            colis
+        };
+        console.log(inputListNew)
+
+        try {
+            dispatch((FormData));
+            Swal.fire({
+                icon: "success",
+                title: "L'operation a reussi avec succès",
+            });
+        } catch (error) {
+            Swal.fire({
+                icon: "error",
+                title: `${error.response.data.message}`,
+                text: "Veuillez verifier vos infor,qtions de connexion",
+            });
+            throw error;
+        }
+    }
     return (
       <>
         <div className='container mt-4'>
@@ -36,11 +72,16 @@ const AddColis = () => {
                     </div>
                     <hr className="my-0" />
                     <div className="card-body">
-                        <form id="formAccountSettings"  method="POST" >
+                        <form 
+                            id="formAccountSettings"  
+                            method="POST" 
+                            
+                            onSubmit={handleSubmitColis}
+                        >
                             <div className="row">
                                 <div className="mb-3 col-md-12">
                                     <label htmlFor="timeZones" className="form-label">NOM AGENT</label>
-                                    <input type="text" className="form-control" />
+                                    <input type="text" placeholder="Alphonsine Banongo" className="form-control" />
                                 </div>
                                 {inputListNew.map((x, i) => {
                                     return (
@@ -50,18 +91,22 @@ const AddColis = () => {
                                         <input
                                             className="form-control"
                                             type="text"
-                                            id="firstName"
-                                            name="firstName"
-                                            autoFocus
+                                            id=""
+                                            name=""
+                                            placeholder="Iphone 14 Pro Max"
+                                            value={x.nom_colis}
+                                            onChange={(e) => handleInputChange(i, "nom_colis", e.target.value)}
                                         />
                                     </div>
                                     <div className="mb-3 col-md-2">
                                         <label htmlFor="number" className="form-label">NOMBRE TOTAL COLIS</label>
                                         <input
                                             className="form-control"
-                                            type="text"
-                                            id="email"
-                                            placeholder=""
+                                            type="number"
+                                            id=""
+                                            placeholder="03"
+                                            value={x.nombre_total_colis}
+                                            onChange={(e) => handleInputChange(i, "nombre_total_colis", e.target.value)}
                                         />
                                     </div>
                                     <div className="mb-3 col-md-2">
@@ -69,8 +114,11 @@ const AddColis = () => {
                                         <input
                                             type="number"
                                             className="form-control"
-                                            id="organization"
-                                            name="organization"
+                                            id=""
+                                            name=""
+                                            placeholder="10 $"
+                                            value={x.prix_unitaire}
+                                            onChange={(e) => handleInputChange(i, "prix_unitaire", e.target.value)}
                                             
                                         />
                                     </div>
@@ -79,9 +127,11 @@ const AddColis = () => {
                                         <input
                                             type="number"
                                             className="form-control"
-                                            id="organization"
-                                            name="organization"
-                                            
+                                            id="kilo_colis"
+                                            name="kilo_colis"
+                                            placeholder="18 Kg"
+                                            value={x.kilo_colis}
+                                            onChange={(e) => handleInputChange(i, "kilo_colis", e.target.value)}
                                         />
                                     </div>
                                     <div className="mb-3 col-md-2">
@@ -92,7 +142,7 @@ const AddColis = () => {
                                                 id=""
                                                 name="prix_total"
                                                 className="form-control"
-                                                placeholder=""
+                                                placeholder="300 $"
                                                 value={x.prix_total}
                                                 onChange={(e) => handleInputChange(i, "prix_total", e.target.value)}
                                             />
@@ -100,7 +150,7 @@ const AddColis = () => {
                                     </div>
                                 
                                     <div className="mb-3 col-md-2">
-                                        <label className="form-label" htmlFor="phoneNumber">AJOUTER COLIS</label>
+                                        <label className="form-label" htmlFor="phoneNumber"></label>
                                         <div className="input-group input-group-merge">
                                         <div className="col-md-2">
                                             {inputListNew.length - 1 === i && (
