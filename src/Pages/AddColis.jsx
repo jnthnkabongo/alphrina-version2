399a51c97f6257a70067b127b5b64000cf5ac09b
jnthnkabongo <jnthnkabongo@gmail.com>
2024-11-02 16:17:08 +0100

    les ajouts

--- src/Pages/AddColis.jsx
+++ src/Pages/AddColis.jsx
@@ -1,178 +1,161 @@
 import { React, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
+import { postColis } from "../actions/ColisAction";
 
 const AddColis = () => {
+
+    const [inputListNew, setInputListNew] = useState([
+        { nom_colis: "", nombre_total_colis: "", prix_unitaire: "", kilo_colis: "", prix_total: "" },
+    ]);
+
     const dispatch = useDispatch();
     const form = useRef();
 
-    const [inputListNew, setInputListNew] = useState([
-        { nom_colis: "", nombre_total_colis:"", prix_unitaire: "", kilo_colis: "", prix_total: ""},
-    ]);
-    
     const handleAddClick = () => {
-        setInputListNew([...inputListNew, { nom_colis: "", nombre_total_colis:"", prix_unitaire:"", kilo_colis:"", prix_total:""}]);
-    }
-    
+        setInputListNew([...inputListNew, { nom_colis: "", nombre_total_colis: "", prix_unitaire: "", kilo_colis: "", prix_total: "" }]);
+    };
+
     const handleInputChange = (index, name, value) => {
         const list = [...inputListNew];
-        list [index][name] = value;
+        list[index][name] = value;
         setInputListNew(list);
-    }
-    
+    };
+
     const handleSubmitColis = async (e) => {
-        e.preventdefault();
-        setloading(true);
-    
+        e.preventDefault();
+
         const colisData = {
-            nom_colis : form.current["nom_colis"].value,
-            nombre_total_colis : form.current["nombre_total_colis"].value,
-            prix_unitaire : form.current["prix_unitaire"].value,
-            kilo_colis : form.current["kilo_colis"].value,
-            prix_total : form.current["prix_total"].value,
+            nomagent: form.current["nom_agent"].value,
+            montantpayer: form.current["montantpayer"].value,
+            totalkilo: form.current["totalkilos"].value,
         };
-        
+
         const colis = inputListNew.map((item) => ({
-            nom_colis: item.colis,
-            nombre_total_colis: item.colis,
-            prix_unitaire: item.colis,
-            kilo_colis: item.colis,
-            prix_total: item.colis
+            nomcolis: item.nom_colis,
+            nbrtotalcolis: item.nombre_total_colis,
+            prixunitaire: item.prix_unitaire,
+            kilocolis: item.kilo_colis,
+            prixtotal: item.prix_total
         }));
 
-        const FormData = {
+        const formDatas = {
             ...colisData,
             colis
         };
-        console.log(inputListNew)
+        const formData = {
+                nomagent: "jonathan@gmail.com",
+                nomcolis: "dd",
+                nbrtotalcolis: "30",
+                prixunitaire: "4",
+                kilocolis: "40",
+                prixtotal: "30",
+                montantpayer: "31",
+                totalkilo: "32",
+        };
 
         try {
-            dispatch((FormData));
+            await dispatch(postColis(formData));
             Swal.fire({
                 icon: "success",
-                title: "L'operation a reussi avec succès",
+                title: "L'opération a réussi avec succès",
             });
+            setInputListNew([{ nom_colis: "", nombre_total_colis: "", prix_unitaire: "", kilo_colis: "", prix_total: "" }]); // Réinitialisation des champs de saisie
         } catch (error) {
             Swal.fire({
                 icon: "error",
                 title: `${error.response.data.message}`,
-                text: "Veuillez verifier vos infor,qtions de connexion",
+                text: "Veuillez vérifier vos informations de connexion.",
             });
             throw error;
         }
-    }
+    };
+
     return (
-      <>
-        <div className='container mt-4'>
+        <>
+            <div className='container mt-4'>
                 <div className="card mb-4">
                     <h5 className="card-header">Faire un enregistrement d'un colis</h5>
-                    <div className="card-body">
-
-                    </div>
+                    <div className="card-body"></div>
                     <hr className="my-0" />
                     <div className="card-body">
-                        <form 
-                            id="formAccountSettings"  
-                            method="POST" 
-                            
-                            onSubmit={handleSubmitColis}
-                        >
+                        <form ref={form} onSubmit={handleSubmitColis}>
                             <div className="row">
                                 <div className="mb-3 col-md-12">
-                                    <label htmlFor="timeZones" className="form-label">NOM AGENT</label>
-                                    <input type="text" placeholder="Alphonsine Banongo" className="form-control" />
+                                    <label htmlFor="nom_agent" className="form-label">NOM AGENT</label>
+                                    <input type="text" id="nom_agent" name="nom_agent" placeholder="Alphonsine Banongo" className="form-control" />
                                 </div>
                                 {inputListNew.map((x, i) => {
-                                    return (
+                                    return(
                                         <>
-                                    <div className="mb-3 col-md-2">
-                                        <label htmlFor="firstName" className="form-label">NOM COLIS</label>
-                                        <input
-                                            className="form-control"
-                                            type="text"
-                                            id=""
-                                            name=""
-                                            placeholder="Iphone 14 Pro Max"
-                                            value={x.nom_colis}
-                                            onChange={(e) => handleInputChange(i, "nom_colis", e.target.value)}
-                                        />
-                                    </div>
-                                    <div className="mb-3 col-md-2">
-                                        <label htmlFor="number" className="form-label">NOMBRE TOTAL COLIS</label>
-                                        <input
-                                            className="form-control"
-                                            type="number"
-                                            id=""
-                                            placeholder="03"
-                                            value={x.nombre_total_colis}
-                                            onChange={(e) => handleInputChange(i, "nombre_total_colis", e.target.value)}
-                                        />
-                                    </div>
-                                    <div className="mb-3 col-md-2">
-                                        <label htmlFor="organization" className="form-label">PRIX UNITAIRE</label>
-                                        <input
-                                            type="number"
-                                            className="form-control"
-                                            id=""
-                                            name=""
-                                            placeholder="10 $"
-                                            value={x.prix_unitaire}
-                                            onChange={(e) => handleInputChange(i, "prix_unitaire", e.target.value)}
-                                            
-                                        />
-                                    </div>
-                                    <div className="mb-3 col-md-2">
-                                        <label htmlFor="organization" className="form-label">KILO COLIS</label>
-                                        <input
-                                            type="number"
-                                            className="form-control"
-                                            id="kilo_colis"
-                                            name="kilo_colis"
-                                            placeholder="18 Kg"
-                                            value={x.kilo_colis}
-                                            onChange={(e) => handleInputChange(i, "kilo_colis", e.target.value)}
-                                        />
-                                    </div>
-                                    <div className="mb-3 col-md-2">
-                                        <label className="form-label" htmlFor="phoneNumber">PRIX TOTAL</label>
-                                        <div className="input-group input-group-merge">
+                                        <div className="mb-3 col-md-2">
+                                            <label className="form-label">NOM COLIS</label>
+                                            <input
+                                                className="form-control"
+                                                type="text"
+                                                placeholder="Iphone 14 Pro Max"
+                                                name="nom_colis"
+                                                value={x.nom_colis}
+                                                onChange={(e) => handleInputChange(i, "nom_colis", e.target.value)}
+                                            />
+                                        </div>
+                                        <div className="mb-3 col-md-2">
+                                            <label className="form-label">NOMBRE TOTAL COLIS</label>
+                                            <input
+                                                className="form-control"
+                                                type="number"
+                                                placeholder="03"
+                                                value={x.nombre_total_colis}
+                                                onChange={(e) => handleInputChange(i, "nombre_total_colis", e.target.value)}
+                                            />
+                                        </div>
+                                        <div className="mb-3 col-md-2">
+                                            <label className="form-label">PRIX UNITAIRE</label>
+                                            <input
+                                                type="number"
+                                                className="form-control"
+                                                placeholder="10 $"
+                                                value={x.prix_unitaire}
+                                                onChange={(e) => handleInputChange(i, "prix_unitaire", e.target.value)}
+                                            />
+                                        </div>
+                                        <div className="mb-3 col-md-2">
+                                            <label className="form-label">KILO COLIS</label>
+                                            <input
+                                                type="number"
+                                                className="form-control"
+                                                placeholder="18 Kg"
+                                                value={x.kilo_colis}
+                                                onChange={(e) => handleInputChange(i, "kilo_colis", e.target.value)}
+                                            />
+                                        </div>
+                                        <div className="mb-3 col-md-2">
+                                            <label className="form-label">PRIX TOTAL</label>
                                             <input
                                                 type="text"
-                                                id=""
-                                                name="prix_total"
                                                 className="form-control"
                                                 placeholder="300 $"
                                                 value={x.prix_total}
                                                 onChange={(e) => handleInputChange(i, "prix_total", e.target.value)}
                                             />
                                         </div>
-                                    </div>
-                                
-                                    <div className="mb-3 col-md-2">
-                                        <label className="form-label" htmlFor="phoneNumber"></label>
-                                        <div className="input-group input-group-merge">
-                                        <div className="col-md-2">
+                                        <div className="mb-3 col-md-2">
                                             {inputListNew.length - 1 === i && (
-                                            <button className="ml10 btn btn-primary" onClick={ handleAddClick}> +</button>
+                                                <button type="button" className="ml10 btn btn-primary" onClick={handleAddClick}>+</button>
                                             )}
                                         </div>
-                                        </div>
-                                    </div>
-                                    </>
+                                        </>
                                     );
                                 })}
                                 <div className="mb-3 col-md-6">
-                                    <label htmlFor="address" className="form-label">MONTANT A PAYER</label>
-                                    <input type="number" className="form-control"/>
+                                    <label htmlFor="montantpayer" className="form-label">MONTANT A PAYER</label>
+                                    <input type="number" id="montantpayer" name="montantpayer" className="form-control" />
                                 </div>
                                 <div className="mb-3 col-md-6">
-                                    <label htmlFor="address" className="form-label">TOTAL KILOS</label>
-                                    <input type="number" className="form-control"/>
+                                    <label htmlFor="totalkilos" className="form-label">TOTAL KILOS</label>
+                                    <input type="number" id="totalkilos" name="totalkilos" className="form-control" />
                                 </div>
-                               
                             </div>
-                             
                             <div className="mt-10">
                                 <button type="submit" className="btn btn-primary me-2">Soumettre</button>
                                 <button type="reset" className="btn btn-outline-secondary">Annuler</button>
@@ -180,8 +163,8 @@ const AddColis = () => {
                         </form>
                     </div>
                 </div>
-        </div>
-      </>
+            </div>
+        </>
     );
 };
 
---
+++ src/actions/ColisAction.js
@@ -0,0 +1,117 @@
+import axioClient from "../axiosClient";
+import Swal from "sweetalert2";
+
+export const postColis = (formData) => {
+  return async (dispatch) => {
+    try {
+      const response = await axioClient.post(`Coli`, formData);
+      Swal.fire({
+        icon: "success",
+        title: `${response.data.message}`,
+      });
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: `${error.response.data.message}`,
+        text: "Veuillez vérifier vos informations de connexion.",
+      });
+      console.log(formData)
+      throw error; // Renvoie l'erreur pour le traitement ultérieur
+    }
+  };
+};
+
+export const getColiId = (id) => {
+  return axioClient
+    .get(`Coli/${id}`)
+    .then((response) => {
+      return response.data.data;
+    })
+    .catch((error) => {
+      if (error.response.status === 404) {
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Erreur lors de la récupération des données",
+          text: `Entre/${error}`,
+        });
+      }
+    });
+};
+
+export const deleteColi = (id) => {
+  return async (dispatch) => {
+    Swal.fire({
+      title: "Êtes-vous sûr?",
+      text: "Vous ne pourrez pas revenir en arrière!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Oui, supprimer!",
+      cancelButtonText: "Annuler",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const response = await axioClient.delete(`Coli/${id}`);
+          Swal.fire({
+            icon: "success",
+            title: `${response.data.message}`,
+          });
+          window.location.reload();
+        } catch (error) {
+          Swal.fire({
+            icon: "error",
+            title: "Erreur lors de la suppression de la dépense",
+            text: `${error.response.data.message}`,
+          });
+          throw error;
+        }
+      }
+    });
+  };
+};
+
+export const putColi = (formData, id) => {
+  return async (dispatch) => {
+    try {
+      const response = await axioClient.put(`Coli/${id}`, formData);
+      Swal.fire({
+        icon: "success",
+        title: `${response.data.message}`,
+      });
+      window.location.href = "/conteneur";
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Erreur lors de la suppression de la dépense",
+        text: `${error.response.data.message}`,
+      });
+      throw error;
+    }
+  };
+};
+
+export const updateColi = (formData, id) => {
+  return async (dispatch) => {
+    try {
+      const response = await axioClient.put(
+        `Coli/${id}`,
+        formData
+      );
+      Swal.fire({
+        icon: "success",
+        title: `${response.data.message}`,
+      });
+      window.location.href = "/conteneur";
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Erreur lors de la modification",
+        text: `${error.response.data.message}`,
+      });
+      throw error;
+    }
+  };
+};
+
