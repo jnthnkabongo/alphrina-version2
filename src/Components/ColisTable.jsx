8a7ee4840366324f5962adbdff307601b5151a4c
jnthnkabongo <jnthnkabongo@gmail.com>
2024-11-03 16:18:45 +0100

    ddelo

--- src/Components/ColisTable.jsx
+++ src/Components/ColisTable.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-
-const ColisTable = ({ nom_agent, id, nom_colis, nbrtotal_colis, prix_unitaire, kilo_colis, prix_total, montant_payer, total_kilo }) => {
-    
+const ColisTable = ({ nom_agent,  id, nom_colis, nbrtotal_colis, prixtotal, prixunitaire, totalkilo, montantpayer }) => {
     return (
         <tr>
-            <td><i className=""></i> <strong>{id}</strong></td>
-            <td><i className=""></i> <strong>{nom_agent}</strong></td>
-            <td><i className=""></i> <strong>{nom_colis}</strong></td>
-            <td><i className=""></i> <strong>{nbrtotal_colis}</strong></td>
+            <td><i className="bx bx-user"></i> <strong>{nom_agent}</strong></td>
+            <td><i className="bx bx-cube"></i> <strong>{nom_colis}</strong></td>
+            <td><i className="bx "></i> <strong>{prixtotal}</strong></td>
+            <td><i className="bx "></i> <strong>{montantpayer}</strong></td>
+            <td><i className="bx"></i> <strong>{prixunitaire}</strong></td>
+            <td><i className="bx"></i> <strong>{nbrtotal_colis}</strong></td>
+            <td><i className="bx"></i> <strong>{totalkilo}</strong></td>
             <td>
-                <Link to="" 
-                ><i className="bx bx-trash fs-2 me-1"></i></Link
-                >
-                <Link to={`/paiementDetteClient/${id}`}
-                ><i className="bx bx-money fs-2 me-1"></i></Link
-                >
+                <Link to={`/supprimerColis/${id}`}>
+                    <i className="bx bx-trash fs-2 me-1"></i>
+                </Link>
+               
             </td>
         </tr>
-    )
-}
+    );
+};
 
-export default ColisTable
+export default ColisTable;
